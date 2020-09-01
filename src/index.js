9eb3249b05008b7bed34155a6401b783a8a79cb5
Aaron Gomez <aaron.gomez1@gmail.com>
2020-08-31 21:00:38 -0700

    edits

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// const imgLoc =
+//   "https://townsquare.media/site/366/files/2018/05/Slayer.jpg?w=980&q=75";
+
+const imgLoc = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
     <h1 className="heading" contentEditable="true" spellCheck="true">
       My Favourite Foods
     </h1>
     <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
+      <img className="slayer-img" src={imgLoc + "?grayscale"} />
+      <img className="slayer-img" src={imgLoc} />
+      <img className="slayer-img" src={imgLoc} />
     </ul>
   </div>,
   document.getElementById("root")
